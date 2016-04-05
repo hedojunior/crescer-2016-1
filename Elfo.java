4f8e125677baf15da257b07abfa391aac26dd113
hedo eccker <hedo eccker>
2016-04-05 20:57:34 -0300

    Elfo

---
+++ Elfo.java
@@ -0,0 +1,40 @@
+
+public class Elfo
+{
+    private String nome;
+    private int experiencia, flechas = 42;
+   
+   public void atirarFlechas(){
+       this.experiencia++;
+              }
+    
+    
+    public Elfo(String nome)
+    {
+        this.nome = nome;
+    }
+ 
+    public void atirarFlechaRefactory(){
+        boolean acertar = true;
+    if(acertar){
+        experiencia++; 
+    }
+    flechas--;
+}
+   
+
+    
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
