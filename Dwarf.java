0d9a9a02d312eddf905bb01cb3dd04fca471aa7d
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-04-06 16:08:55 -0300

    feat(Dwarf): adicionar nome e setters/getters para nome.

--- Dwarf.java
+++ Dwarf.java
@@ -3,12 +3,19 @@ public class Dwarf {
     private int vida;
     private String nome;
 
-     public Dwarf(String nome){
+    public Dwarf(String nome){
          this.vida = 110;
          this.nome = nome;
-     }
+    }
     
-     public void danoRecebido() {
-        this.vida -= 10;
-     }
+    public void danoRecebido() {
+         this.vida -= 10;
+    }
+     
+    public void setNome(String novoNome) {
+        nome = novoNome;
+    }
+    public String getNome () {
+        return nome;
+    }
 }
