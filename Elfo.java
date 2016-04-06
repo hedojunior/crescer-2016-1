bb26a71d5dfe593da08fac7854161a3c15391b3e
hedo eccker <hedo eccker>
2016-04-05 22:45:16 -0300

    Identação + código atirarDwarf

--- Elfo.java
+++ Elfo.java
@@ -1,40 +1,28 @@
+public class Elfo {
+	private String nome;
+	private int experiencia, flechas = 42;
 
-public class Elfo
-{
-    private String nome;
-    private int experiencia, flechas = 42;
-   
-   public void atirarFlechas(){
-       this.experiencia++;
-              }
-    
-    
-    public Elfo(String nome)
-    {
-        this.nome = nome;
-    }
- 
-    public void atirarFlechaRefactory(){
-        boolean acertar = true;
-    if(acertar){
-        experiencia++; 
-    }
-    flechas--;
+	public void atirarFlechas() {
+		this.experiencia++;
+	}
+
+	public Elfo(String nome) {
+		this.nome = nome;
+	}
+
+	public void atirarFlechaRefactory() {
+		boolean acertar = true;
+		if (acertar) {
+			experiencia++;
+		}
+		flechas--;
+	}
+
+	public void atirarFlechaDwarf( Dwarf nome) {
+		flechas--;
+		int vida = nome.getVida() - 10;
+		nome.setVida(vida);
+
+	}
+
 }
-   
-
-    
-}
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
