a0c610034f8de4ef5cec0bfbe628b86267560953
hedo eccker <hedo eccker>
2016-04-07 22:17:21 -0300

    feat(Dwarf): Inserir checagem que verifica o Status do Dwarf, onde ele recebe dano caso o status seja 'VIVO' e não recebe caso seja 'MORTO', além de no segundo caso sua vida não se tornar menor do que 0. test(DwarfTest): Inserir  novos testes quanto à troca de Status quando ocorre a "morte" do Dwarf, quanto à criação de Dwarfs com 120 de vida e inserção de divisões para aderir ao modo AAA.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -2,26 +2,43 @@ public class Dwarf {
     
     private int vida;
     private String nome;
-
+    private Status status;
+    
     public Dwarf(String nome){
          this.vida = 110;
          this.nome = nome;
-    }
+         this.status = Status.VIVO;
+        }
     
     public int getVida() {
         return vida;
     }
     
     public void danoRecebido() {
-         this.vida -= 10;
+        if(this.status != Status.MORTO) { 
+            this.vida -= 10;
+                if (vida <= 0){
+                    this.estaMorto();
+                }
+        }
     }
      
     public void setNome(String novoNome) {
         nome = novoNome;
     }
+    
     public String getNome () {
         return nome;
     }
+    
+    public Status getStatus() {
+        return status;
+    }
+    
+    private void estaMorto(){
+        this.status = Status.MORTO;
+        this.vida = 0;
+    }        
 }
 
 
--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -9,24 +9,43 @@ public class DwarfTest
 {
    @Test
    public void criarDwarf110vida(){
-       Dwarf dwarf = new Dwarf("Dwarf");
+       //Arrange
        int vida_esperada = 110;
+       //Act
+       Dwarf dwarf = new Dwarf("Dwarf");
+       //Assert 
        assertEquals(vida_esperada, dwarf.getVida());  
    }
    
    @Test
-   public void dwarfTomaDano(){
+   public void criarDwarf120vida(){
+       //Arrange
+       int vida_esperada = 120;
+       boolean obtido;
+       //Act
+       Dwarf dwarf = new Dwarf("Dwarf");
+       obtido = vida_esperada == dwarf.getVida();
+       //Assert 
+       assertFalse(obtido);  
+   }
+   
+   @Test
+   public void dwarfToma10Dano(){
+       //Arrange
        Dwarf dwarf = new Dwarf("Dwarf");
        int vida_esperada = dwarf.getVida() - 10;
+       //Act
        dwarf.danoRecebido();
-       int vida_obtida = dwarf.getVida();
-       assertEquals(vida_esperada,vida_obtida);
+       //Assert
+       assertEquals(vida_esperada,dwarf.getVida());
    }
    
    @Test
    public void dwarfToma110Dano(){
+       //Arrange
        Dwarf dwarf = new Dwarf("Dwarf");
        int vida_esperada = dwarf.getVida() - 110;
+       // Act
        dwarf.danoRecebido();
        dwarf.danoRecebido();
        dwarf.danoRecebido();
@@ -38,7 +57,52 @@ public class DwarfTest
        dwarf.danoRecebido();
        dwarf.danoRecebido();
        dwarf.danoRecebido();
-       int vida_obtida = dwarf.getVida();
-       assertEquals(vida_esperada,vida_obtida);
+       // Assert
+       assertEquals(vida_esperada,dwarf.getVida());
+   }
+      
+   @Test
+   public void estaMortoCom110Dano(){
+       //Arrange
+       Dwarf dwarf = new Dwarf("Dwarf");
+       Status esperado = Status.MORTO;
+       //Act
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       //Assert
+       assertEquals(0, dwarf.getVida());
+       assertEquals(esperado, dwarf.getStatus());
+   }
+   
+   @Test
+   public void estaMortoCom120Dano(){
+       //Arrange
+       Dwarf dwarf = new Dwarf("Dwarf");
+       Status esperado = Status.MORTO;
+       //Act
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       dwarf.danoRecebido();
+       //Assert
+       assertEquals(0, dwarf.getVida());
+       assertEquals(esperado, dwarf.getStatus());
    }
 }
