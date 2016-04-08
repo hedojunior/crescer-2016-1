610e09954109586c10bab937ccaeb3cca0ee5efc
hedo eccker <hedo eccker>
2016-04-08 12:43:18 -0300

    test(DwarfTest): refatorar teste e deixá-lo não-comentado.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -267,20 +267,21 @@ public class DwarfTest
        assertEquals(xp_esperada, d1.getExperiencia());
    }
    
-   /* @Test
+   @Test
    public void vidaEExperienciaIguaisAposDanoMeirelesNewString() {
        DataTerceiraEra data = new DataTerceiraEra(01,01,2015);
        Dwarf d1 = new Dwarf("Meireles",data);
        int vida_esperada = d1.getVida();
        int xp_esperada = d1.getExperiencia();
+       String nome_esperado = "Meireles";
        //Act
        d1.danoRecebido();
        //Assert
        assertEquals(vida_esperada, d1.getVida());
        assertEquals(xp_esperada, d1.getExperiencia());
+       assertEquals(nome_esperado, d1.getNome());
    }
-   */
-   
+      
    @Test
    public void vidaEExperienciaAposDanoDataBissextoDwarf90Vida() {
        DataTerceiraEra data = new DataTerceiraEra(01,01,2016);
