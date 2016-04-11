d7ef5dbb72ae39684dde8f49ef6419b1d244e76d
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-04-11 17:32:34 -0300

    feat(Personagem): criar classe-pai com todas as informações que são comuns à todas as classes.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Personagem.java
@@ -0,0 +1,37 @@
+public class Personagem
+{
+    protected String nome;
+    protected int experiencia;
+    protected Inventario inventario = new Inventario();
+    
+    public Personagem (String nome)
+    {
+        this.nome = nome;
+    }
+    
+    public String getNome()
+    {
+        return this.nome;
+    }
+    
+    public int getExperiencia()
+    {
+        return this.experiencia;
+    }
+    
+    public Inventario getInventario()
+    {
+        return this.inventario;
+    }
+    
+    public void adicionarItem(Item item) 
+    {
+        this.getInventario().adicionarItem(item);
+    }
+    
+    public void perderItem(Item item)
+    {
+        this.getInventario().removerItem(item);
+    }
+    
+}
