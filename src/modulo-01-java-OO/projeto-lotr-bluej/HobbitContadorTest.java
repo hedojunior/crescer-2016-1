05107a9e755aba3e4d574237c00a4daef9230c6b
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-04-11 14:03:13 -0300

    test(HobbitContadorTest): Adicionar testes para o método obterMultiplosDeTresAte(); Refatorar testes adicionando anotações AAA.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
@@ -7,33 +7,68 @@ public class HobbitContadorTest
 {
     @Test
     public void SeORetornoDoValorVaiSerCorretoCom3ArraysInternos() {
+        //Arrange
+        HobbitContador contador = new HobbitContador();
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        //Act
         arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
         arrayDePares.add(new ArrayList(Arrays.asList(28,7)));
         arrayDePares.add(new ArrayList(Arrays.asList(33,27)));
-        HobbitContador contador = new HobbitContador();
+        //Assert
         assertEquals(822,contador.calcularDiferenca(arrayDePares));
     }
     
     @Test
     public void SeORetornoDoValorVaiSerCorretoCom1ArrayInterno() {
-        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
-        arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
+        //Arrange
         HobbitContador contador = new HobbitContador();
+        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        //Act
+        arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
+        //Assert
         assertEquals(60,contador.calcularDiferenca(arrayDePares));
     }
     
     @Test
     public void SeORetornoDoValorVaiSerCorretoCom5ArraysInternos() {
-        int esperado;
+        //Arrange
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
+        HobbitContador contador = new HobbitContador();
+        //Act
         arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
         arrayDePares.add(new ArrayList(Arrays.asList(28,7)));
         arrayDePares.add(new ArrayList(Arrays.asList(33,27)));
         arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
         arrayDePares.add(new ArrayList(Arrays.asList(12,6)));
-        HobbitContador contador = new HobbitContador();
+        //Assert
         assertEquals(942,contador.calcularDiferenca(arrayDePares));
     }
     
+   @Test
+   public void seRetornaOValorCorretoComParametroSendo100() {
+        //Arrange
+        HobbitContador contador = new HobbitContador();
+        //Act
+        //Assert
+        assertEquals(99,contador.obterMaiorMultiploDeTresAte(100));    
+   }
+   
+   @Test
+   public void seRetornaOValorCorretoComParametroSendo3() {
+        //Arrange
+        HobbitContador contador = new HobbitContador();
+        //Act
+        //Assert
+        assertEquals(3,contador.obterMaiorMultiploDeTresAte(3));    
+   }
+   
+   @Test
+   public void seRetornaOValorCorretoComParametroSendo0() {
+        //Arrange
+        HobbitContador contador = new HobbitContador();
+        //Act
+        //Assert
+        assertEquals(0,contador.obterMaiorMultiploDeTresAte(0));    
+   }
 }
+
