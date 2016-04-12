37cd3627744334b9f147f535b328c42165db3788
hedo eccker <hedo eccker>
2016-04-12 08:24:05 -0300

    refactor(Personagem): mudar tipo do atributo 'vida' de int para double. Mudança necessária para atender necessidade do cliente.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Personagem.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Personagem.java
@@ -1,50 +1,41 @@
 public class Personagem
 {
     protected String nome;
-    protected int experiencia, vida;
+    protected double vida;
+    protected int experiencia;
     protected Status status;
-    
     protected Inventario inventario = new Inventario();
     
-    public Personagem (String nome)
-    {
+    public Personagem (String nome) {
         this.nome = nome;
         this.status = Status.VIVO;
     }
     
-    public String getNome()
-    {
+    public String getNome() {
         return this.nome;
     }
     
-    public int getExperiencia()
-    {
+    public int getExperiencia() {
         return this.experiencia;
     }
     
-    public int getVida()
-    {
+    public double getVida() {
         return this.vida;
     }
    
-    public Status getStatus()
-    {
+    public Status getStatus() {
         return this.status;
     }
     
-    public Inventario getInventario()
-    {
+    public Inventario getInventario() {
         return this.inventario;
     }
     
-    public void adicionarItem(Item item) 
-    {
+    public void adicionarItem(Item item) {
         this.getInventario().adicionarItem(item);
     }
     
-    public void perderItem(Item item)
-    {
+    public void perderItem(Item item) {
         this.getInventario().removerItem(item);
     }
-    
 }
