392d54d30864ac815b2cb9316520ce7e16d6aa86
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-04-12 15:40:54 -0300

    feat(Item): adicionar (sobreescrever) método equals.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Item.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Item.java
@@ -18,4 +18,11 @@ public class Item {
     public String getDescricao() {
         return this.descricao;
     }    
+    
+    public boolean equals(Object obj) {
+        Item outro = (Item)obj;
+        return this.getQuantidade() == outro.getQuantidade() &&
+            this.getDescricao() != null && outro !=null && 
+            this.descricao.equals(outro.getDescricao());
+    }
 }
