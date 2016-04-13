1814f7e9a926c2de01f55312e7844f03304c1cd8
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-04-13 17:22:56 -0300

    feat(DicionarioSindarin): criar classe DicionarioSindarin que têm exemplos de HashMap.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DicionarioSindarin.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class DicionarioSindarin {
+    public static void main(String[] args) {
+        HashMap<String, String> dicionarioSindarin = new HashMap<>(); 
+        dicionarioSindarin.put("terra", "amar");
+        dicionarioSindarin.put("fogo", "naur");
+        dicionarioSindarin.put("vento", "gwaew");
+        dicionarioSindarin.put("água", "nem");
+        dicionarioSindarin.put("coração", "gûr");
+       //Obter um valor a partir de uma chave
+        String terraEmSindarin = dicionarioSindarin.get("terra");
+       //Sobrepor o valor de uma detemrinada chave 
+        dicionarioSindarin.put("água","nîn");
+       //Remover chave-valor
+       dicionarioSindarin.remove("água");
+       //Está vazio?
+       boolean estaVazio = dicionarioSindarin.isEmpty();
+       //Tamanho
+       int tamanhoDePares = dicionarioSindarin.size();
+       //Contém chave?
+       boolean contemAgua = dicionarioSindarin.containsKey("água");
+       //Contém valor?
+       boolean contemNaur = dicionarioSindarin.containsValue("naur");
+       
+       for(String chave : dicionarioSindarin.keySet()) {
+           System.out.println(chave);
+       }
+       
+       int qtdNaur = 0;
+       for (String valor : dicionarioSindarin.values()) {
+           if(valor.equals("naur")){
+           qtdNaur++;
+        }
+       }
+       System.out.println("Quantos tem naur: " + qtdNaur);
+       qtdNaur = 0;
+       for (Map.Entry<String, String> chaveValor: dicionarioSindarin.entrySet()){
+           if(chaveValor.getValue().equals("naur")){
+               qtdNaur++;
+               System.out.println("A chave " + chaveValor.getKey() + " significa naur");
+           }
+        }
+        System.out.println("Quantos tem naur: " + qtdNaur);
+    
+        dicionarioSindarin.put("FB", "facebook");
+        dicionarioSindarin.put("Ea", "badoo");
+        System.out.println("-------------------------------------------------------------");
+       for (Map.Entry<String, String> chaveValor: dicionarioSindarin.entrySet()){
+           System.out.println("K: " + chaveValor.getKey() + "- V: " + chaveValor.getValue());
+       } 
+    }
+}
