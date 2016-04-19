1f06a4eadd0effbdd0d9484d1e18c2e3c7448dd4
hedo eccker <hedo eccker>
2016-04-19 20:19:26 -0300

    fix(Inventario): Remover try catch e retornar ao modo que apenas retorna null quando o ArrayList passado é vazio.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -39,20 +39,14 @@ public class Inventario {
    public Item maiorQtd() {
        int maior_quantidade = 0;
        int indice_maior = 0;
-       
+       if(this.inventario.isEmpty()) return null;
        for(int i = 0; i <this.inventario.size(); i++) {
            if(this.inventario.get(i).getQuantidade() > maior_quantidade){
                maior_quantidade = this.inventario.get(i).getQuantidade();
                indice_maior = i;
             }
        }
-       try {
-           return this.inventario.get(indice_maior);
-       }
-       catch(IndexOutOfBoundsException e) {
-          System.out.println("O Inventário está vazio!");
-          return null;
-       }
+       return this.inventario.get(indice_maior);
    }
    
     public void ordenaItens() {
