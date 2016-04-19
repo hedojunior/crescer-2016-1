46a739c8f7a23b7e25f69ac07ca6d34252d44b36
hedo eccker <hedo eccker>
2016-04-19 09:02:13 -0300

    refactor(Estrategia1): modificar classe Estrategia1 para remover redundâncias e refatorar condições.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia1.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Estrategia1.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Estrategia1 implements Estrategia {
-        private static ArrayList<Elfo> OrdemDoUltimoAtaque = new ArrayList<>();
+        private ArrayList<Elfo> ordemDoUltimoAtaque = new ArrayList<>();
         
         public void estrategiaDeAtaque(ArrayList<Elfo> horda, ArrayList<Dwarf> alvos) {
-            OrdemDoUltimoAtaque.clear();
+            ordemDoUltimoAtaque.clear();
             double intencoes = 0;
             int numeroDwarves = alvos.size();
             intencoes = this.calcularIntencoes(horda, numeroDwarves);
@@ -11,39 +11,25 @@ public class Estrategia1 implements Estrategia {
         }
         
         public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
-            return this.OrdemDoUltimoAtaque;
+            return this.ordemDoUltimoAtaque;
         }
         
         public double calcularIntencoes (ArrayList<Elfo> horda, int numeroDwarves){
-            int contadorVerde = 0, contadorNoturno = 0;
-            for (Elfo elfo : horda) {
-                if(elfo instanceof ElfoVerde) {
-                    contadorVerde++;
-                } else {
-                    contadorNoturno++;
-                }
-            }
-            return ((contadorVerde + contadorNoturno) * numeroDwarves);
+            return (horda.size() * numeroDwarves);
         }
         
         private void batalha (ArrayList<Elfo> horda, ArrayList<Dwarf> alvos, double intencoes) {
-            double intencoesElfoNoturno = Math.floor(intencoes * 0.3);
+            int intencoesElfoNoturno = (int)(intencoes * 0.3);
+            boolean classeElfo;
             for (Elfo elfo : horda) {
-                if(elfo instanceof ElfoNoturno && intencoesElfoNoturno > 0 || elfo instanceof ElfoVerde) {
-                    OrdemDoUltimoAtaque.add(elfo);
+                classeElfo = elfo instanceof ElfoNoturno;
+                if(classeElfo && intencoesElfoNoturno > 0 || !classeElfo) {
+                    ordemDoUltimoAtaque.add(elfo);
                     for(Dwarf dwarf : alvos){
-                        if (elfo instanceof ElfoNoturno && intencoesElfoNoturno > 0){
-                            elfo.atirarFlecha(dwarf);
-                            intencoesElfoNoturno--;
-                        }   else if (elfo instanceof ElfoVerde){
-                                elfo.atirarFlecha(dwarf);
-                            }
-                    }
-                    } else {
-                        continue;
+                        elfo.atirarFlecha(dwarf);
+                        if(classeElfo)intencoesElfoNoturno--;
+                    }  
                 }
             }
         }
-        
-        
 }
