11cdeb8c9a462a1bcbd5c157cb31e0ac9df6d989
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-06 18:05:37 -0300

    feat(folder<modulo-5-dotnet>: adicionar pasta do módulo 5 com início do projeto do Megaman. Contém as classes (Robo.cs, Megaman.cs)

---
+++ src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Megaman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaMegaman
+{
+    class Megaman : Robo
+    {
+
+        public Megaman() : base()
+        {
+            PoderDeAtaque = 6;
+        }
+
+        protected override string Nome
+        {
+            get
+            {
+                return "Megaman";
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Robo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaMegaman
+{   
+    public abstract class Robo
+    {
+        public Robo()
+        {
+            Vida = 100;
+            PoderDeAtaque = 5;
+            PoderDeDefesa = 0;
+        }
+
+
+
+        protected abstract string Nome { get;}
+        
+        public int Vida { get; set; }
+  
+        protected int PoderDeAtaque { get; set; }
+        
+        protected int PoderDeDefesa { get; set; }
+    
+        public virtual void Atacar(Robo robo);
+    }
+    
+}
