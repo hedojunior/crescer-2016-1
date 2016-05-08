62b45d5e491b317ad09471d780ebe42a925ccac4
hedo eccker <hedo eccker>
2016-05-08 10:44:07 -0300

    feat(Protoman.cs): adicionar classe Protoman que implementa e sobreescreve alguns métodos da classe pai.

---
+++ src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Protoman.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaMegaman
+{
+    public class Protoman : Robo
+    {
+        
+        private int NumeroMortes { get; set; }
+
+        public Protoman() : base()
+        {
+            PoderDeDefesa = 2;
+        }
+
+        public override string Nome
+        {
+            get
+            {
+               return "Protoman";
+            }
+        }
+
+        public override void ReceberDano(int poderDeAtaque)
+        {
+            var aux = PoderDeDefesa;
+            EquiparUpgradesParaBatalha();
+            var dano = poderDeAtaque - PoderDeDefesa;
+
+            if (dano > 0) Vida -= dano;
+
+            if (Vida <= 0 && NumeroMortes == 0)
+            {
+                Vida = 20;
+                NumeroMortes = 1;
+                PoderDeAtaque = 7;
+            }
+            else if (Vida <= 0) { Vida = 0; }
+            PoderDeDefesa = aux;
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            var aux = PoderDeAtaque;
+            EquiparUpgradesParaBatalha();
+            robo.ReceberDano(PoderDeAtaque);
+            PoderDeAtaque = aux;
+        }
+
+    }
+}
