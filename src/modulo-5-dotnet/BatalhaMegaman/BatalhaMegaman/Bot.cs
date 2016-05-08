123da1677549234fb9cba94836048a0d1a987de7
hedo eccker <hedo eccker>
2016-05-08 10:41:25 -0300

    feat(Bot.cs): adicionar classe Bot que implementa os métodos da classe pai Robo.

---
+++ src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Bot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaMegaman
+{
+    public class Bot : Robo
+    {
+
+        public Bot() : base()
+        {
+
+        }
+
+        public override string Nome
+        {
+            get
+            {
+                return "Bot";
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            var aux = PoderDeAtaque;
+            EquiparUpgradesParaBatalha();
+            robo.ReceberDano(PoderDeAtaque);
+            PoderDeAtaque = aux;
+        }
+
+        public override void ReceberDano(int poderDeAtaque)
+        {
+            var aux = PoderDeDefesa;
+            EquiparUpgradesParaBatalha();
+            var dano = poderDeAtaque - PoderDeDefesa;
+            if (dano > 0) Vida -= dano;
+            PoderDeDefesa = aux;
+        }
+    }
+}
