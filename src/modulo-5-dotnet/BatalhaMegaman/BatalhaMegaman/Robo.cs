ff205f81646d0effc34ff396ed7513e0ac374329
hedo eccker <hedo eccker>
2016-05-08 10:24:51 -0300

    feat(Robo.cs): adicionar métodos ReceberDano, ToString, EquiparUpgrade e EquiparUpgradesParaBatalha.

--- src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Robo.cs
+++ src/modulo-5-dotnet/BatalhaMegaman/BatalhaMegaman/Robo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +16,40 @@ namespace BatalhaMegaman
             PoderDeDefesa = 0;
         }
 
-
-
-        protected abstract string Nome { get;}
+        public abstract string Nome { get;}
         
-        public int Vida { get; set; }
+        public List<IUpgrade> upgrades = new List<IUpgrade>();
+            
+        protected int Vida { get; set; }
   
         protected int PoderDeAtaque { get; set; }
         
         protected int PoderDeDefesa { get; set; }
-    
-        public virtual void Atacar(Robo robo);
+
+        public virtual void Atacar(Robo robo) { }
+        
+        public virtual void ReceberDano(int PoderDeAtaque) { }    
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + ", Vida: " + Vida + ", Ataque: "
+                + PoderDeAtaque + ", Defesa: " + PoderDeDefesa;
+        }
+        
+                
+        public void EquiparUpgrade(IUpgrade upgrade)
+        {
+            if (upgrades.Count < 3) upgrades.Add(upgrade);
+        }
+
+        public void EquiparUpgradesParaBatalha()
+        {
+            foreach (IUpgrade upgrade in upgrades)
+            {
+                PoderDeAtaque += upgrade.BonusDeAtaque;
+                PoderDeDefesa += upgrade.BonusDeDefesa;
+            }
+         
+        }
     }
-    
 }
