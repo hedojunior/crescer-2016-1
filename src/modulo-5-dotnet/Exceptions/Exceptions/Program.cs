f7bd6bb79dddbb07d7e7937c8dbb67c444650527
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-11 14:02:32 -0300

    feat(Content): adicionar arquivos de configuração do projeto e estilos dos colegas para o desafio do ZenGarden.

---
+++ src/modulo-5-dotnet/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Digite um número:");
+                var numero = Console.ReadLine();
+
+                var numeroConvertido = Convert.ToInt32(numero);
+
+                if (numeroConvertido == 13)
+                    throw new ArgumentException("Você digitou o número do azar");
+
+                Console.WriteLine("O número convertido é: " + numeroConvertido);
+
+            } 
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número!");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("O número deve estar no intervalo de -243683648 e 243683648");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
