8777e281221c50d47b072dfd7c1f5b3ff6963b0e
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-11 14:01:42 -0300

    feat(DesafioController.cs): adicionar action que retorna a view ZenGarden, como proposto no desafio.

--- src/modulo-5-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/DesafioController.cs
+++ src/modulo-5-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/DesafioController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.Models;
+
 
 namespace MVC.Controllers
 {
     public class DesafioController : Controller
     {
         // GET: Desafio
-        public ActionResult Index( string aluno = null )
+        public ActionResult ZenGarden(string aluno)
         {
+            ViewBag.aluno = aluno;
             return View();
         }
     }
