7ebb351a48f68e71c7184c6eb8fe86190f9a248f
hedo eccker <hedo eccker>
2016-05-11 12:44:55 -0300

    chore(\MVC): adicionar projeto MVC com resolução do exercício proposto número 1.

---
+++ src/modulo-5-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
@@ -0,0 +1,40 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StreetFighterController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult SobreMim()
+        {
+            var eu = new SobreMimModel
+            {
+                Nome = "Hedo Eccker da Silva Júnior",
+                Cidade = "São Leopoldo/RS",
+                LingPref = "Portugol",
+                ComidaPref = "Mac'n'Cheese",
+                LivroPref = "Eu sou a Lenda",
+                JogoPref = "FIFA 16",
+                SeriadoPref = "Breaking Bad",
+                FilmePref = "Um estranho no ninho",
+                MateriaPref = "Geografia"
+            };
+
+            return View(eu);
+        }
+    }
+}
---
+++ src/modulo-5-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string LingPref { get; set; }
+        public string ComidaPref { get; set; }
+        public string LivroPref { get; set; }
+        public string JogoPref { get; set; }
+        public string SeriadoPref { get; set; }
+        public string FilmePref { get; set; }
+        public string MateriaPref { get; set; }
+    }
+}
