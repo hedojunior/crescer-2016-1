8697ad7ea270d765b5a853abc8f16b1bfe34bb52
hedo eccker <hedo eccker>
2016-05-14 22:06:27 -0300

    feat(PedidoController.cs): adicionar controller para o site como um todo, com todas as funções propostas no Gist.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Controllers/PedidoController.cs
@@ -0,0 +1,76 @@
+﻿using LojaNinja.Dominio;
+using LojaNinja.MVC.Models;
+using LojaNinja.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public RepositorioVendas repo = new RepositorioVendas();
+
+        // GET: Produto
+        public ActionResult CadastrarPedido()
+        {
+            return View();
+        }
+
+        public ActionResult SalvarPedido(PedidoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var pedido = new Pedido(model.DataEntrega, model.Nome, model.ValorVenda, model.Pagamento, model.NomeCliente, model.Cidade, model.Estado);
+                //if (model.Id.HasValue)
+                //    repositorio.AtualizarPedido(pedido);
+                //else
+                repo.IncluirPedido(pedido);
+                return View("Sucesso", model);
+            }
+            else return View("CadastrarPedido", model);
+
+        }
+
+        public ActionResult ExcluirPedido(int id)
+        {
+            repo.ExcluirPedido(id);
+
+            return View("Mensagem");
+        }
+
+        public ActionResult EditarPedido(int id)
+        {
+            var pedido = repo.ObterPedidoPorId(id);
+            var model = new PedidoModel(pedido);
+            repo.ExcluirPedido(pedido.Id);
+            return View("CadastrarPedido", model);
+
+        }
+
+        public ActionResult ListaPedidos(string cliente, string produto)
+        {
+            List<Pedido> pedidos;
+            if (cliente == null && produto == null)
+            {
+                pedidos = repo.ObterPedidos();
+            }
+            else
+            {
+                pedidos = repo.ObterPedidosPorNomeEProduto(cliente, produto);
+            }
+            return View(pedidos);
+
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var pedido = repo.ObterPedidoPorId(id);
+            var model = new PedidoModel(pedido);
+            return View("Sucesso", model);
+        }
+    }
+}
