556b312f6b5828d3f8a0b443d50155bab43da41e
hedo eccker <hedo eccker>
2016-05-15 14:51:14 -0300

    refactor(RouteConfig.cs): mudar rota default para setar a página Home como principal.

--- src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/App_Start/RouteConfig.cs
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LojaNinja.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Pedido", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
