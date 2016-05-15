b0a5844716483619cd27d68e8ae2c0ae8f1318c4
hedo eccker <hedo eccker>
2016-05-15 14:58:50 -0300

    test(UnitTest1.cs): adicionar casos de teste para a camada de domínio.

--- src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio.Test/UnitTest1.cs
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LojaNinja.Dominio;
 
 namespace LojaNinja.Dominio.Test
 {
@@ -7,8 +8,35 @@ namespace LojaNinja.Dominio.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CriaPedidoSemIDCorretamente()
         {
+
+            var pedido = new Pedido(new DateTime(2018,10,20,20,54,20),"Kunai",1200,TipoPagamento.Diners,"Hedo","SL","RS");
+            Assert.AreEqual("Hedo",pedido.NomeCliente);
+            Assert.AreEqual("RS", pedido.Estado);
+            Assert.AreEqual("SL", pedido.Cidade);
+            Assert.AreEqual(TipoPagamento.Diners, pedido.TipoPagamento);
+            Assert.AreEqual(1200, pedido.Valor);
+            Assert.AreEqual("Kunai", pedido.NomeProduto);
+            Assert.AreEqual(new DateTime(2018,10,20,20,54,20), pedido.DataEntregaDesejada);
         }
+
+        [TestMethod]
+        public void CriaPedidoComIDCorretamente()
+        {
+
+            var pedido = new Pedido(1, new DateTime(2018, 10, 20, 20, 54, 19),new DateTime(2018, 10, 20, 20, 54, 20), "Kunai", 1200, TipoPagamento.Diners, "Hedo", "SL", "RS",true);
+            Assert.AreEqual("Hedo", pedido.NomeCliente);
+            Assert.AreEqual(true, pedido.PedidoUrgente);
+            Assert.AreEqual(1, pedido.Id);
+            Assert.AreEqual("RS", pedido.Estado);
+            Assert.AreEqual("SL", pedido.Cidade);
+            Assert.AreEqual(TipoPagamento.Diners, pedido.TipoPagamento);
+            Assert.AreEqual(1200, pedido.Valor);
+            Assert.AreEqual("Kunai", pedido.NomeProduto);
+            Assert.AreEqual(new DateTime(2018, 10, 20, 20, 54, 19), pedido.DataPedido);
+            Assert.AreEqual(new DateTime(2018, 10, 20, 20, 54, 20), pedido.DataEntregaDesejada);
+        }
+
     }
 }
