20abdce612c9a27d70e497e093fb639d3e9141c3
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-16 17:44:36 -0300

    feat(PermissoesUser): adicionar enum de permissões do Usuário.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio/PermissoesUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Dominio
+{
+   public enum PermissoesUser
+    {
+        COMUM,
+        ADMIN
+    }
+}
