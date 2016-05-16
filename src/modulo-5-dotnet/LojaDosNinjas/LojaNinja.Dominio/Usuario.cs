e94a7bae1dbba59bf6166644c31cd5034d3c5167
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-16 17:44:06 -0300

    feat(Usuario,UsuarioServico): adicionar classe Usuario e classe UsuarioServico, que faz validações de senha e busca no repositório.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Dominio
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public PermissoesUser Permissoes { get; set; }
+
+        public Usuario(string nome, string senha, string email)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Senha = senha;
+            Permissoes = PermissoesUser.COMUM;
+        }
+
+        public Usuario() { }
+
+    }
+}
---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio/UsuarioServico.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNinja.Dominio
+{
+    class UsuarioServico
+    {
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioServico(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+     public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+     {
+            string senhaCriptografada = Criptografar(senha);
+
+            Usuario usuarioEncontrado = _usuarioRepositorio.BuscarUsuarioPorAutenticacao(email, senhaCriptografada);
+
+            return usuarioEncontrado;
+     }
+
+     private string Criptografar(string texto)
+     {
+            //TODO: MELHORAR CRIPTOGRAFIA
+        using (MD5 md5Hash = MD5.Create())
+        {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+        }
+     }
+
+    }
+}
