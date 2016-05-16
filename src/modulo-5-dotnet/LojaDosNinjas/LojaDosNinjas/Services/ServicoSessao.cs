f153cb927adf8dcf3355352b620be72ced846e11
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-16 17:42:38 -0300

    feat(ServicoSessao): adicionar classe de serviço que gerencia a sessão do usuário.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Services/ServicoSessao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Services
+{
+    public static class ServicoSessao
+    {
+        private const string COOKIE_AUTENTICACAO = "COOKIE_AUTENTICACAO";
+        private const string USUARIO_LOGADO = "USUARIO_LOGADO";
+
+        private static Dictionary<string, string> _usuariosLogados = new Dictionary<string, string>();
+
+        public static UsuarioLogadoModel UsuarioLogado
+        {
+            get
+            {
+                return (UsuarioLogadoModel)HttpContext.Current.Session[USUARIO_LOGADO];
+            }
+        }
+    }
+}
