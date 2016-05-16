4cecaecd0507d4cf117dff619bf952b19b6f1b60
Hedo Eccker da Silva Junior <hedo.junior@cwinet.local>
2016-05-16 17:42:07 -0300

    feat(CadastroModel,UsuarioLogadoModel): adicionar models para uso na autenticação do usuário.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Models/Login/CadastroModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models.Login
+{
+    public class CadastroModel
+    {
+        [Required]
+        [DisplayName("E-mail")]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Senha")]
+        [StringLength(100)]
+        public string Senha { get; set; }
+
+        [Required]
+        [DisplayName("Senha")]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        public string Permissoes { get; set; }
+    }
+}
