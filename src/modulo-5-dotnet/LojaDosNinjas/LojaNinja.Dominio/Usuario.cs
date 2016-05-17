1ef799451ad1040e3942891b9910b32442876a9c
hedo eccker <hedo eccker>
2016-05-17 02:29:37 -0300

    refactor(Usuario.cs): mudanças internas ao método construtor do Usuário.

--- src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio/Usuario.cs
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaNinja.Dominio/Usuario.cs
@@ -8,17 +8,19 @@ namespace LojaNinja.Dominio
 {
     public class Usuario
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Senha { get; set; }
         public string Email { get; set; }
-        public PermissoesUser Permissoes { get; set; }
+        public List<string> Permissoes { get; set; }
 
         public Usuario(string nome, string senha, string email)
         {
+            this.Id = 0;
             this.Nome = nome;
             this.Email = email;
             this.Senha = senha;
-            Permissoes = PermissoesUser.COMUM;
+            Permissoes.Add("COMUM");
         }
 
         public Usuario() { }
