8f90cf309e51c8fb645be09664423b72b2322f42
hedo eccker <hedo eccker>
2016-05-17 02:39:32 -0300

    feat(NovoUsuarioModel): adicionar model para cadastro de novo usuário.

---
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Models/Login/NovoUsuarioModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models.Login
+{
+    public class NovoUsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public List<string> Permissoes { get; set; }
+
+
+        public NovoUsuarioModel (int id, string nome, string email, string senha)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Permissoes.Add("COMUM");
+        }
+
+        public NovoUsuarioModel() { }
+    }
+}
