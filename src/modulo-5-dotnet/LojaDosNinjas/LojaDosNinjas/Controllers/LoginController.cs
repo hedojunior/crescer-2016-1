9b1db123a8408e9dbfebf8b39176445ae319e1ae
hedo eccker <hedo eccker>
2016-05-17 02:23:07 -0300

    feat(LoginController.cs): adicionar toda a mecânica de Autenticação e autorização de usuários.

--- src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Controllers/LoginController.cs
+++ src/modulo-5-dotnet/LojaDosNinjas/LojaDosNinjas/Controllers/LoginController.cs
@@ -4,20 +4,67 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LojaNinja.MVC.Models.Login;
+using LojaNinja.Dominio;
+using Repositorio;
+using LojaNinja.MVC.Services;
+using LojaNinja.MVC.Filters.BaseAulaSeguranca.Filters;
 
 namespace LojaNinja.MVC.Controllers
 {
     public class LoginController : Controller
     {
+        private UsuarioServico _usuarioServico;
+
+        public LoginController()
+        {
+            _usuarioServico = new UsuarioServico(
+                    new UsuarioRepositorioADO()
+                );
+        }
+
         // GET: Usuario
+        [HttpGet]
         public ActionResult Index()
+        {
+            if (!ServicoSessao.EstaLogado)
+            {
+                return View();
+            }
+            return View("Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Entrar(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario usuarioEncontrado =
+                    _usuarioServico.BuscarUsuarioPorAutenticacao(
+                            loginViewModel.Email, loginViewModel.Senha);
+
+                if (usuarioEncontrado.Email != null)
+                {
+                    var usuarioLogadoModel = new UsuarioLogadoModel(usuarioEncontrado);
+
+                    ServicoSessao.CriarSessao(usuarioLogadoModel);
+                    UsuarioLogadoModel usuarioLogado = ServicoSessao.UsuarioLogado;
+                    ViewBag.NomeUsuarioLogado = usuarioLogado.Nome;
+
+                    return View("Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("INVALID_USER", "Usuário ou senha inválido.");
+                }
+            }
+
+            return View("Index", loginViewModel);
+        }
+
+        public ActionResult Cadastro()
         {
             return View();
         }
-
-        //public ActionResult Entrar(LoginViewModel model)
-        //{
-        //     throw NotImplementedException;
-        //}
     }
 }
