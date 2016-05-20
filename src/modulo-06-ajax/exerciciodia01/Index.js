2c8b2af12f23d585e19e8e65d5349344566d2af3
hedo eccker <hedo eccker>
2016-05-19 23:20:15 -0300

    feat(exerciciodia01): adicionar resolução dos exercícios propostos no dia 1 de Ajax. --- TODO: Estilizar páginas.

---
+++ src/modulo-06-ajax/exerciciodia01/Index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function carregarIronMaiden() {
+
+  $.ajax({ url: urlSpotifyGet, type: 'GET' })
+  .then(
+    function onSuccess(res) {
+      var $lista = $('#ulDoHeavyMetal')
+      apendarImagens(res, $lista);
+    }
+  );
+};
+//------------------
+
+$('#pesquisaEcletica').click(function(e){
+
+  var valorPesquisa = $('#TermoPesquisa').val();
+
+  var chance = Math.random();
+  var trocarUrl = true;
+  if(chance <= 0.20) trocarUrl = false;
+
+  debugger;
+  var urlValorPesquisa = 'https://api.spotify.com/v1/search?q=' + valorPesquisa +'&type=artist';
+
+  $.get({
+    url: urlValorPesquisa,
+    success: function(res){
+      var id = res.artists.items[0].id;
+      var urlArtistaPorId = checarSeEhJustinBieber(trocarUrl, valorPesquisa, id);
+      $.get({
+        url: urlArtistaPorId,
+        success: function(res) {
+          var $lista = $('#lista');
+          apendarImagens(res, $lista);
+        },
+
+        error: function(xhr, err){
+          if (xhr.status === 400)
+          alert('Caro usuário, devido a um erro ' + xhr.status + ', não foi possível pesquisar por ' + valorPesquisa);
+        }
+      });
+    }
+  });
+});
+
+//uteis
+
+function checarSeEhJustinBieber(trocarUrl, valorPesquisa, id) {
+  if (trocarUrl === true && valorPesquisa === "Justin Bieber")
+  return 'https://api.spotify.com/v1/artists/douchebag/albums?limit=50';
+  else
+  return 'https://api.spotify.com/v1/artists/' + id + '/albums?limit=50';
+}
+//---------------
+function apendarImagens(res, $lista) {
+  res.items.forEach(function(cover){
+    $lista
+    .append($('<li>')
+    .append($('<img>').attr('src',cover.images[1].url)));
+  });
+}
