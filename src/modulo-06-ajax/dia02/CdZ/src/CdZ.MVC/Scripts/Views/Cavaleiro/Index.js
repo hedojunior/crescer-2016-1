222c416217fe716793eb335b27fb3965d0d0a6fc
hedo eccker <hedo eccker>
2016-05-22 01:27:25 -0300

    feat(Index.js): adicionar método que gera as notificações quando um novo cavaleiro é inserido, algumas mudanças oriundas do projeto dos instrutores e refatoração do código.

--- src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Index.js
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Index.js
@@ -1,20 +1,25 @@
 ﻿'use strict';
 
 var ultimoId;
-
-function carregarDadosNaPagina() {
+var $cavaleiros = $('#cavaleiros');
+function carregarCavaleirosNaPagina () {
     $.ajax({ url: urlCavaleiroGet, type: 'GET' })
-    .then(
+    .done(
         function onSuccess(res) {
             console.log(res.data);
-            var $cavaleiros = $('#cavaleiros');
+            console.log('potatO');
             ultimoId = res.data[res.data.length - 1].Id;
+            console.log(ultimoId);
             res.data.forEach(function (cava) {
                 $cavaleiros.append(
                     $('<li>').append(cava.Nome)
+                    .append($('<button>')
+                                        .attr('data-cavaleiro-id', cava.Id)
+                                        .click(excluirCavaleiroNoServidor)
+                                        .text('Excluir'))
                 );
             });
-          },
+        },
         function onError(res) {
             console.error(':(');
             console.error(res);
@@ -33,103 +38,108 @@ function carregarDadosNaPagina() {
         console.log('acabouuuuuuuu');
     });
 };
-carregarDadosNaPagina();
+
+carregarCavaleirosNaPagina();
 
 function appendarNovos() {
     $.ajax({
-        url: urlCavaleiroGet, type: 'GET'
-    })
-    .done(function (res) {
+        url: urlCavaleiroGet,
+        type: 'GET'
+    }).done(function (res) {
+        console.log('batata 46');
+        var contadorNovosCavaleiros = 0;
+            
         res.data.forEach(function (cava) {
             if (cava.Id > ultimoId) {
-                $('#cavaleiros').append(
-                    $('<li>').append(cava.Nome)
-
+                $cavaleiros
+                .append($('<li>')
+                        .append(cava.Nome)
+                            .append($('<button>')
+                                        .attr('data-cavaleiro-id', cava.Id)
+                                        .click(excluirCavaleiroNoServidor)
+                                        .text('Excluir'))
                 );
+                contadorNovosCavaleiros++;
+                ultimoId = cava.Id;
             }
         });
+        if (contadorNovosCavaleiros > 0) adicionarNotificação(contadorNovosCavaleiros);
     });
 };
- 
-setTimeout(appendarNovos, 5000);
 
-function criarHtmlCavaleiro(cava) {
-    return $('<li>')
-        .append(cava.Nome)
-        .append(
-            $('<button>')
-                .attr('data-cavaleiro-id', cava.Id)
-                .click(excluirCavaleiroNoServidor)
-                .text('Excluir')
-        );
-}
+    var recarregarLista = setInterval(appendarNovos, 5000);
 
-function excluirCavaleiroNoServidor() {
-    alert('Excluído com sucesso!');
-    var cavaleiroId = parseInt($(this).attr('data-cavaleiro-id'));
+    function adicionarNotificação(numeroCavasAdicionados) {
+        Notification
+        .requestPermission()
+        .then(function(result) {
+            if(result === 'granted'){
+                var frasePronta = numeroCavasAdicionados === 1 ? 
+                    numeroCavasAdicionados + ' cavaleiro foi adicionado!' : numeroCavasAdicionados + ' cavaleiros foram adicionados!';
 
-    $.ajax({
-        url: '/Cavaleiro/Delete' + $.param({ id: cavaleiroId }),
-        type:'DELETE'
-    });
-      
-        
-}
+                var options = {
+                    
+                    body: frasePronta,
+                    icon: 'http://bit.ly/1TmHq3E'
+                }
+                new Notification('',options);
+            }    
+        }
+        )};
 
-function registrarEventoDoBotao() {
-    $('#btnCriar').click(function () {
+    function criarHtmlCavaleiro(cava) {
+        return $('<li>')
+            .append(cava.Nome)
+            .append(
+                $('<button>')
+                    .attr('data-cavaleiro-id', cava.Id)
+                    .click(excluirCavaleiroNoServidor)
+                    .text('Excluir')
+            );
+    }
+
+    function excluirCavaleiroNoServidor() {
+        alert('Excluído com sucesso!');
+        var cavaleiroId = parseInt($(this).attr('data-cavaleiro-id'));
 
         $.ajax({
-            url: urlCavaleiroPost,
-            type: 'POST',
-            data: {
-                Nome: 'Xiru ' + new Date().getTime(),
-                AlturaCm: 187,
-                Signo: 7,
-                TipoSanguineo: 1,
-                DataNascimento: new Date(),
-                Golpes: ['Cólera do Dragão', 'Cólera dos 100 dragões'],
-                LocalNascimento: {
-                    Texto: 'Beijing'
-                },
-                LocalTreinamento: {
-                    Texto: '5 picos de rosan'
-                },
-                Imagens: [{
-                    Url: 'http://images.uncyc.org/pt/3/37/Shiryumestrepokemon.jpg',
-                    IsThumb: true
-                }, {
-                    Url: 'http://images.uncyc.org/pt/thumb/5/52/Shyryugyarados.jpg/160px-Shyryugyarados.jpg',
-                    IsThumb: false
-                }]
-            }
+            url: '/Cavaleiro/Delete',
+            type: 'DELETE',
+            data: { id: cavaleiroId }
         });
 
-    });
-};
-registrarEventoDoBotao();
 
+    }
 
+    function registrarEventoDoBotao() {
+        $('#btnCriar').click(function () {
 
-/*.done(function (res) {
-    console.log(res.data);
-        var $cavaleiros = $('#cavaleiros');
-        res.data.forEach(function (cava) {
-            $cavaleiros.append(
-                $('<li>').append(cava.Nome)
-            );
+            $.ajax({
+                url: urlCavaleiroPost,
+                type: 'POST',
+                data: {
+                    Nome: 'Xiru ' + new Date().getTime(),
+                    AlturaCm: 187,
+                    Signo: 7,
+                    TipoSanguineo: 1,
+                    DataNascimento: new Date(),
+                    Golpes: ['Cólera do Dragão', 'Cólera dos 100 dragões'],
+                    LocalNascimento: {
+                        Texto: 'Beijing'
+                    },
+                    LocalTreinamento: {
+                        Texto: '5 picos de rosan'
+                    },
+                    Imagens: [{
+                        Url: 'http://images.uncyc.org/pt/3/37/Shiryumestrepokemon.jpg',
+                        IsThumb: true
+                    }, {
+                        Url: 'http://images.uncyc.org/pt/thumb/5/52/Shyryugyarados.jpg/160px-Shyryugyarados.jpg',
+                        IsThumb: false
+                    }]
+                }
+            });
+
         });
-})
-.fail(function (res) {
-    console.error(':(');
-    console.error(res);
-
-    var criarSpanComErro = function (msg) {
-        return $('<span>').text(msg).addClass('erro');
     };
-
-    $('#feedback')
-    .append(criarSpanComErro(res.status))
-    .append($('<br>'))
-    .append(criarSpanComErro(res.statusText));
-});*/
+    registrarEventoDoBotao();
