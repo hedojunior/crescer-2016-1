3260749abb0c8e1e1f8b63fe1eba646a89db9b97
hedo eccker <hedo eccker>
2016-05-23 11:51:48 -0300

    feat(CavaleiroViewModel): adicionar mudanças de acordo com o projeto dos instrutores.

--- src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Models/Cavaleiro/CavaleiroViewModel.cs
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Models/Cavaleiro/CavaleiroViewModel.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using Dominio = CdZ.Dominio;
+using System.Web.Script.Serialization;
+using CdZ.MVC.Extensions;
 
 namespace CdZ.MVC.Models.Cavaleiro
 {
@@ -12,25 +14,30 @@ namespace CdZ.MVC.Models.Cavaleiro
         public string Nome { get; set; }
         public double AlturaCm { get; set; }
         public double PesoLb { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public string DataNascimento { get; set; }
+        [ScriptIgnore]
+        public DateTime DataNascimentoObj
+        {
+            get { return DataNascimento.FromISOToDateTime(); }
+            set { DataNascimento = value.FromDateTimeToISOString(); }
+        }
         public Signo Signo { get; set; }
         public TipoSanguineo TipoSanguineo { get; set; }
-        public Local LocalNascimento { get; set; }
-        public Local LocalTreinamento { get; set; }
-        public IList<string> Golpes { get; set; }
+        public LocalViewModel LocalNascimento { get; set; }
+        public LocalViewModel LocalTreinamento { get; set; }
+        public IList<GolpeViewModel> Golpes { get; set; }
         public IList<ImagemViewModel> Imagens { get; set; }
-        public CavaleiroViewModel()
-        {
-            // TODO: corrigir model-bind
-            DataNascimento = DateTime.Today;
-        }
+
+        public CavaleiroViewModel() { }
 
         public Dominio.Cavaleiro ToModel()
         {
-            var golpesObj = Golpes.Select(_ => new Golpe(_)).ToList();
+            var golpesObj = Golpes.Select(_ =>_.ToModel()).ToList();
             var imagensObj = Imagens.Select(_ => _.ToModel()).ToList();
-
-            return new Dominio.Cavaleiro(Nome, AlturaCm, PesoLb, DataNascimento, Signo, TipoSanguineo, LocalNascimento, LocalTreinamento, golpesObj, imagensObj);
+             
+            return new Dominio.Cavaleiro
+                (Nome, AlturaCm, PesoLb, DataNascimentoObj, Signo, TipoSanguineo, 
+                LocalNascimento.ToModel(), LocalTreinamento.ToModel(), golpesObj, imagensObj);
         }
     }
 }
