bd85340892820dbdd8648a1c05cd8f48d47f641d
hedo eccker <hedo eccker>
2016-05-23 11:59:26 -0300

    feat(Models): adicionar view models de golpe e local.

---
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Models/Cavaleiro/GolpeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CdZ.MVC.Models.Cavaleiro
+{
+    public class GolpeViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+
+        public Dominio.Golpe ToModel()
+        {
+            return new Dominio.Golpe(Id, Nome);
+        }
+    }
+
+}
---
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Models/LocalViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CdZ.MVC.Models
+{
+    public class LocalViewModel
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+
+        public Dominio.Local ToModel()
+        {
+            return new Dominio.Local(Id, Texto);
+        }
+    }
+}
