de29075d55ec1d8b9fe3e898960516fb4715f794
Hedo <hedojunior@gmail.com>
2016-06-10 10:21:24 -0300

    feat(MeuSQLUtils): adicionar exportação de csv.

--- src/modulo-8-java/Exercicios0906/src/main/java/MeuSQLUtils.java
+++ src/modulo-8-java/Exercicios0906/src/main/java/MeuSQLUtils.java
@@ -1,14 +1,19 @@
 
 import br.com.hedo.aula3.ConnectionUtils;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,11 +25,16 @@ public class MeuSQLUtils {
     private static final String SELECTCSV = "SELECT * FROM CIDADE";
 
     public static void main(String[] args) {
-        MeuSQLUtils sql = new MeuSQLUtils();
+//        MeuSQLUtils sql = new MeuSQLUtils();
         try (Scanner teclado = new Scanner(System.in)) {
-            System.out.println("Digite o nome do arquivo:");
-            String filename = teclado.nextLine();
-            sql.importCSV(filename);
+            System.out.println("Digite qualquer tecla para iniciar a exportação:");
+            String tecla = teclado.nextLine();
+            if(!tecla.toLowerCase().matches("^select\\s.*$"))
+                System.out.println("Não é select");
+            else
+                System.out.println("É select");
+//            System.out.println("Aguarde, exportando o arquivo para CSV...");
+//            sql.exportCSV();
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -58,6 +68,15 @@ public class MeuSQLUtils {
         }
     }
 
+    public void readSQLCommand(String SQLCommand){
+        boolean ehSelect = false;
+        if(SQLCommand.toLowerCase().matches("^select\\s.*$"))
+            ehSelect = true;
+        else {
+            
+        }
+    }
+    
     public void importCSV(String csvFileName) {
         if (!csvFileName.matches("^.*\\.(csv|CSV)$")) {
             System.out.println("O arquivo não é .csv");
@@ -93,11 +112,29 @@ public class MeuSQLUtils {
         try (final Connection connection = ConnectionUtils.getConnection()) {
             try (Statement st = connection.createStatement()) {
                 try (ResultSet rs = st.executeQuery(SELECTCSV)) {
-                    String id = String.valueOf(rs.getInt("IDCidade"));
-                    String nome = rs.getString("Nome");
-                    String uf = rs.getString("UF");
-                    String concatenado = id + ";" + nome + ";" + uf + ";";
-                    
+                    ArrayList<String> linhas = new ArrayList<String>();
+                    while (rs.next()) {
+                        String id = String.valueOf(rs.getInt("IDCidade"));
+                        String nome = rs.getString("Nome");
+                        String uf = rs.getString("UF");
+                        String concatenado = id + ";" + nome + ";" + uf + ";";
+                        linhas.add(concatenado);
+                    }
+                    File csv = new File("export.csv");
+                    try (final Writer writer = new FileWriter(csv)) {
+                        try (final BufferedWriter bfWriter = new BufferedWriter(writer)) {
+                            for(int i = 0; i < linhas.size(); i++){
+                                bfWriter.write(linhas.get(i));
+                                bfWriter.flush();
+                                bfWriter.newLine();
+                            }
+                            System.out.println("Arquivo exportado com sucesso.");
+                        } catch (Exception e) {
+                            System.err.println(e.getMessage());
+                        }
+                    } catch (Exception e) {
+                        System.err.println(e.getMessage());
+                    }
                 } catch (SQLException e) {
                     System.err.println(e.getMessage());
                 }
